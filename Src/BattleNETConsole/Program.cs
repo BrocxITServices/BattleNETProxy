809b7f6cce5269a9abdaebb81c3969bc8bccdb29
Dani <danislaysou@outlook.com>
2024-01-25 21:20:10 +0100

    setup

---
+++ Src/BattleNETConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Headers;
+
+internal class Program
+{
+    const string clientId= "";
+    const string clientSecret = "";
+    private static void Main(string[] args)
+    {
+        var token = GetGameToken();
+
+        Console.WriteLine("Hello, World!");
+    }
+
+    private static string GetGameToken()
+    {
+        var client = new HttpClient();
+
+        client.BaseAddress = new Uri("https://oauth.battle.net/authorize");
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", "Basic");
+        var content = new FormUrlEncodedContent(new[]
+        {
+        new KeyValuePair<string, string>("grant_type", "client_credentials")
+        });
+
+
+
+        throw new NotImplementedException();
+    }
+}
